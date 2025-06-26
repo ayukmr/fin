4647fa036c09d580230268cd65618871f226b4c6
Ayush Kumar <github@ayukmr.com>
2025-06-25 23:13:15 -0700

    Input ticker instead of CIK

--- src/fin/__init__.py
+++ src/fin/__init__.py
@@ -10,9 +10,31 @@ def main():
         exit()
 
     if len(sys.argv) >= 3:
+        headers = {'User-Agent': 'example@example.com'}
+
+        data = requests.get(
+            'https://www.sec.gov/files/company_tickers.json',
+            headers=headers
+        ).json()
+
+        tickers = list(data.values())
+
+        cik = next(
+            (
+                str(t['cik_str']).zfill(10)
+                for t in tickers
+                if t['ticker'] == sys.argv[2]
+            ),
+            None
+        )
+
+        if not cik:
+            print('error: invalid ticker')
+            exit()
+
         facts = requests.get(
-            f'https://data.sec.gov/api/xbrl/companyfacts/CIK{sys.argv[2]}.json',
-            headers={'User-Agent': '-'}
+            f'https://data.sec.gov/api/xbrl/companyfacts/CIK{cik}.json',
+            headers=headers
         ).json()
     else:
         with open('data.json') as f:
